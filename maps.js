0047d5ab9e8f1bd8172c3c399b1726564fc5b6ad
alirtaskiran <alirtaskiran@gmail.com>
2024-02-20 20:12:46 +0300

    size-for-key-value

--- maps.js
+++ maps.js
@@ -1,21 +1,19 @@
-let myMap = new Map();
+let myMap = new Map(); // => MAP OLUSTURMA
 
-console.log(typeof myMap);
+console.log(typeof myMap); // => OBJECT DONUYOR
 
-const key1 = "Ali Rıza";
-const key2 = {a:10, b:20};
+const key1 = "Ali Rıza";    // KEYLER OLUSTURULDU
+const key2 = { a: 10, b: 20 };
 const key3 = () => 2;
 
-// SET
+// SET                      
 myMap.set(key1, "String Değer");
 myMap.set(key2, "Object Literal Değer");
 myMap.set(key3, "Function Değer");
 
 console.log(myMap);
 
-
 // GET
-
 console.log(key1);
 console.log(myMap.get(key1));
 
@@ -24,3 +22,39 @@ console.log(myMap.get(key2));
 
 console.log(key3);
 console.log(myMap.get(key3));
+
+// LENGTH OGRENME
+console.log(myMap.size);
+
+// FOREACH ILE GEZINME
+
+myMap.forEach(function (value, key) {
+  console.log(key, value);
+});
+
+// FOR OF ILE GEZINME
+
+for (let [key, value] of myMap) {
+  // => DESTRUCTURING
+  console.log(key, value);
+}
+
+// KEYLER UZERINDE GEZINME
+
+for (let key of myMap.keys()) {
+  console.log(key);
+}
+
+// VALUELAR UZERINDE GEZINME
+
+for (let value of myMap.values()) {
+  console.log(value);
+}
+
+// ARRAYDEN MAP OLUSTURMA
+
+const array = [["Key1", "Value1"], ["Key2", "Value2"]]
+
+let myArr = new Map(array);
+
+console.log(myArr);
