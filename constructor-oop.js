ce0f21df7bdbb95781283bab9a3d26791a54dd21
alirtaskiran <alirtaskiran@gmail.com>
2024-02-25 11:00:03 +0300

    constructor'e giris

--- constructor-oop.js
+++ constructor-oop.js
@@ -1,26 +1,43 @@
-function Employee (name, age, salary){
-    this.name = name;
-    this.age = age;
-    this.salary = salary;
+// function Employee (name, age, salary){
+//     this.name = name;
+//     this.age = age;
+//     this.salary = salary;
 
-    this.showInfos = function(){
-        console.log(this.name, this.age, this.salary);
-    }
-}
+//     this.showInfos = function(){
+//         console.log(this.name, this.age, this.salary);
+//     }
+// }
 
-const employee1 = new Employee();
-const employee2 = new Employee();
+// const employee1 = new Employee();
+// const employee2 = new Employee();
 
-employee1.name = "Ali Rıza";
-employee1.age = 27;
-employee1.salary = 30000;
+// employee1.name = "Ali Rıza";
+// employee1.age = 27;
+// employee1.salary = 30000;
 
-employee2.name = "Mahmut";
-employee2.age = 50;
-employee2.salary = 70000;
+// employee2.name = "Mahmut";
+// employee2.age = 50;
+// employee2.salary = 70000;
 
-employee1.showInfos();
-employee2.showInfos();
+// employee1.showInfos();
+// employee2.showInfos();
 
-console.log(employee1);
-console.log(employee2);
+// console.log(employee1);
+// console.log(employee2);
+
+
+function Car(marka, model, yil, tip) {
+    this.marka = marka;
+    this.model = model;
+    this.yil = yil;
+    this.tip = tip;
+  }
+  
+  const araba1 = new Car("BMW", "M5", 2015, "Sedan");
+  const araba2 = new Car("Audi", "Q7 ", 2016, "SUV");
+  const araba3 = new Car("Mercedes", "C180", 2017, "Sedan");
+  
+  
+  console.log(araba1);
+  console.log(araba2);
+  console.log(araba3);
--- map-filter-foreach.js
+++ map-filter-foreach.js
@@ -1,112 +1,113 @@
-const students = [
-    {
-        fullName: "Berkay Turna",
-        age: 25,
-        isDeleted: false,
-        isActive: true,
-        courses: ["JavaScript", "React", "Node.js"],
-        instructors: [
-            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
-            { fullName: "Hicran Ertugral", topics: ["TypeScript", "Angular"] },
-        ]
-    },
-    {
-        fullName: "Cenk Grid Kaynak",
-        age: 32,
-        isDeleted: false,
-        isActive: true,
-        courses: ["JavaScript", "React Native", ".Net Core"],
-        instructors: [
-            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
-            { fullName: "Korhan Kosece", topics: [".Net Core"] },
-        ]
-    },
-    {
-        fullName: "Ali Riza Taskiran",
-        age: 24,
-        isDeleted: false,
-        isActive: true,
-        courses: ["Java", "Golang"],
-        instructors: [
-            { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
-            { fullName: "Buse Seker", topics: ["Problem Solving"] },
-        ]
-    },
-    {
-        fullName: "Mert Kirant",
-        age: 32,
-        isDeleted: false,
-        isActive: true,
-        courses: ["Expressjs", "Node.js"],
-        instructors: [
-            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
-            { fullName: "Onder Tarım", topics: ["React Native"] },
-        ]
-    },
-    {
-        fullName: "Firat Can Tas",
-        age: 26,
-        isDeleted: false,
-        isActive: false,
-        courses: ["Expressjs", "Node.js", ".Net Core"],
-        instructors: [
-            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
-            { fullName: "Onder Tarım", topics: ["React Native"] },
-        ]
-    },
-    {
-        fullName: "Baris Peker",
-        age: 38,
-        isDeleted: true,
-        isActive: false,
-        courses: ["JavaScript", "HTNL"],
-        instructors: [
-            { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
-            { fullName: "Onder Tarım", topics: ["React Native"] },
-        ]
-    },
-]
+// const students = [
+//     {
+//         fullName: "Berkay Turna",
+//         age: 25,
+//         isDeleted: false,
+//         isActive: true,
+//         courses: ["JavaScript", "React", "Node.js"],
+//         instructors: [
+//             { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//             { fullName: "Hicran Ertugral", topics: ["TypeScript", "Angular"] },
+//         ]
+//     },
+//     {
+//         fullName: "Cenk Grid Kaynak",
+//         age: 32,
+//         isDeleted: false,
+//         isActive: true,
+//         courses: ["JavaScript", "React Native", ".Net Core"],
+//         instructors: [
+//             { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//             { fullName: "Korhan Kosece", topics: [".Net Core"] },
+//         ]
+//     },
+//     {
+//         fullName: "Ali Riza Taskiran",
+//         age: 24,
+//         isDeleted: false,
+//         isActive: true,
+//         courses: ["Java", "Golang"],
+//         instructors: [
+//             { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+//             { fullName: "Buse Seker", topics: ["Problem Solving"] },
+//         ]
+//     },
+//     {
+//         fullName: "Mert Kirant",
+//         age: 32,
+//         isDeleted: false,
+//         isActive: true,
+//         courses: ["Expressjs", "Node.js"],
+//         instructors: [
+//             { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//             { fullName: "Onder Tarım", topics: ["React Native"] },
+//         ]
+//     },
+//     {
+//         fullName: "Firat Can Tas",
+//         age: 26,
+//         isDeleted: false,
+//         isActive: false,
+//         courses: ["Expressjs", "Node.js", ".Net Core"],
+//         instructors: [
+//             { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//             { fullName: "Onder Tarım", topics: ["React Native"] },
+//         ]
+//     },
+//     {
+//         fullName: "Baris Peker",
+//         age: 38,
+//         isDeleted: true,
+//         isActive: false,
+//         courses: ["JavaScript", "HTNL"],
+//         instructors: [
+//             { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//             { fullName: "Onder Tarım", topics: ["React Native"] },
+//         ]
+//     },
+// ]
 
-// //forEach
-// const forEachResult = students.forEach(
-//     (student, index, arr) => {
-//         console.log(`${student.fullName}`);
-//         // console.log(arr);
-//         return student;
-//     }
+// // //forEach
+// // const forEachResult = students.forEach(
+// //     (student, index, arr) => {
+// //         console.log(`${student.fullName}`);
+// //         // console.log(arr);
+// //         return student;
+// //     }
+// // );
+
+// // console.log(forEachResult); /* undefined */
+
+// // // map
+// // const newStudents = students.map(
+// //     student => {
+// //        return student;
+// //     }
+// // )
+// // students[0].fullName = "AHMET"
+// // console.log(students[0].fullName);
+// // console.log(students[0]);
+// // console.log(newStudents);
+// // console.log(newStudents.length);
+// // console.log(students);
+
+
+
+// //filter
+// console.log(
+//     students.filter(student => {
+//         if (student.age < 30) {
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     })
+// );
+// console.log(
+//     students.filter(student => student.age < 30)
 // );
 
-// console.log(forEachResult); /* undefined */
-
-// // map
-// const newStudents = students.map(
-//     student => {
-//        return student;
-//     }
-// )
-// students[0].fullName = "AHMET"
-// console.log(students[0].fullName);
-// console.log(students[0]);
-// console.log(newStudents);
-// console.log(newStudents.length);
-// console.log(students);
-
-
-//filter
-console.log(
-    students.filter(student => {
-        if (student.age < 30) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    })
-);
-console.log(
-    students.filter(student => student.age < 30)
-);
-
-console.log(
-    students.filter(student => !student.isDeleted && student.isActive)
-);
+// console.log(
+//     students.filter(student => !student.isDeleted && student.isActive)
+// );
