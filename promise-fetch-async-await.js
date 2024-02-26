bc45a3738c24c5ef9de6a30d464f2b34a6e9c8d1
alirtaskiran <alirtaskiran@gmail.com>
2024-02-27 00:40:29 +0300

    add new promise example

--- promise-fetch-async-await.js
+++ promise-fetch-async-await.js
@@ -30,3 +30,23 @@
 //   });
 
 // console.log(promise);
+
+
+const myPromise = new Promise((resolve, reject) => {
+  // Uzun süren bir işlem gerçekleştiriyoruz (örneğin bir API'den veri çekiyoruz)
+  setTimeout(() => {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) { // Şartın sağlanıp sağlanmadığını rastgele belirliyoruz
+      resolve(randomNumber); // Başarılı durum
+    } else {  // Şart sağlanmazsa hata fırlatıyoruz
+      reject(new Error('Bir hata oluştu')); // Başarısız durum
+    }
+  }, 1000); 
+});
+
+// Promise tamamlandığında çalışacak işlemler
+myPromise.then((result) => {
+  console.log('İşlem başarılı:', result);
+}).catch((error) => {
+  console.error('İşlem başarısız:', error);
+});
