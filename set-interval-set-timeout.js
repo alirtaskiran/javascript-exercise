27df4b1175d8feec70271d79f93b0d66d9d3b1cf
alirtaskiran <alirtaskiran@gmail.com>
2024-03-03 00:08:27 +0300

    use setInterval

--- set-interval-set-timeout.js
+++ set-interval-set-timeout.js
@@ -3,3 +3,10 @@ console.log(this);
 setTimeout((name) => { // Belirlediğimiz süre sonunda bir kere çalışır.
     console.log(`Merhaba ${name}`);
 }, 2000, 'Ahmet');
+
+let interval = 0;
+
+setInterval(() => { // Belirlediğimiz süre sonunda sürekli çalışır.
+    console.log(`Interval = ${interval}`);
+    interval++;
+}, 3000);
