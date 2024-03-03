7cd01fec271492c095fc211078377d47b780fb86
alirtaskiran <alirtaskiran@gmail.com>
2024-03-03 17:48:40 +0300

    lesson ended!

---
+++ dom.js
@@ -0,0 +1,8 @@
+// onload(); // bu fonksiyon sayfa yüklendiğinde çalışır.
+
+document.querySelector("h1").innerHTML = "Welcome to my page!"; // h1 etiketini (tek olarak) seçer ve içeriğini değiştirir.
+document.querySelectorAll("li")[2].innerHTML= "Amazon" // li etiketlerini seçer ve 3. sıradakini değiştirir.
+
+document.querySelector("#btn").style.backgroundColor = "red";
+
+
