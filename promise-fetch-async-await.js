6d21cab1857b726256f35281dd6b7a932a79803d
alirtaskiran <alirtaskiran@gmail.com>
2024-03-11 21:19:27 +0300

    post method added!

--- promise-fetch-async-await.js
+++ promise-fetch-async-await.js
@@ -74,13 +74,46 @@
 
 class Request {
   get(url) {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.warn(err));
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
   }
 }
 
-const request = new Request();
-request.get("https://jsonplaceholder.typicode.com/posts");
+const request1 = new Request();
+let albums;
+request1
+  .get("https://jsonplaceholder.typicode.com/albums")
+  .then((data) => {
+    albums = data;
+    console.log(albums);
+  })
+  .catch((err) => console.warn(err));
 
+
+const request2 = new Request();
+
+request2.post("https://jsonplaceholder.typicode.com/albums", {
+  userId: 1,
+  title: "Thriller",
+})
+  .then((newAlbum) => console.log(newAlbum))
+  .catch((err) => console.warn(err));
